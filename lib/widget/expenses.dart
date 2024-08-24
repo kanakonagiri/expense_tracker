b193723111f9ea8d7ae90200530f0bd788f5a43a
kanakonagiri <nagiri@6-inc.com>
2024-08-24 14:37:55 +0900

    add: expense

--- lib/widget/expenses.dart
+++ lib/widget/expenses.dart
@@ -1,5 +1,6 @@
 import 'package:expense_tracker/widget/expenses_list/expenses_list.dart';
 import 'package:expense_tracker/model/expense.dart';
+import 'package:expense_tracker/widget/new_expense.dart';
 import 'package:flutter/material.dart';
 
 class Expenses extends StatefulWidget {
@@ -29,10 +30,7 @@ class _ExpensesState extends State<Expenses> {
 
   void _openAddExpenseOverlay() {
     showModalBottomSheet(
-        context: context,
-        builder: (ctx) {
-          return const Text('Add Expense');
-        });
+        context: context, builder: (ctx) => const NewExpense());
   }
 
   @override
